d0ca6fba09cc7c88c1eb27096fb5e443410bdb6f
sjyu1988 <sjyu1988@gmail.com>
2016-11-26 21:46:48 -0500

    add getfreeport. Always find a free port to run. add a README.md to DataBrowser, describing how to set your shell startup configuration file (e.g., cshrc and bashrc).

--- DataBrowser/FSview/script_imfit.py
+++ DataBrowser/FSview/script_imfit.py
@@ -34,6 +34,78 @@ if os.path.exists('CASA_imfit_args.json'):
     with open(imgdir + 'CASA_imfit_out', 'w') as fp:
         pickle.dump(out, fp)
 
+# dspecDF_tmp = pd.DataFrame({'time': xx - xx[0],
+#                             'freq': yy,
+#                             'timestr': timestrs,
+#                             'dspec': tab2_spec_plt.flatten(),
+#                             'fits_local': fits_local,
+#                             'fits_global': fits_global})
+# 'x_pos': [ll[0][1] for ll in Gauss_params],
+# 'y_pos': [ll[0][2] for ll in Gauss_params],
+# 'x_width': [ll[0][3] for ll in Gauss_params],
+# 'y_width': [ll[0][4] for ll in Gauss_params],
+# 'amp_gaus': [ll[0][0] for ll in Gauss_params],
+# 'theta': [ll[0][5] for ll in Gauss_params],
+# 'amp_offset': [ll[0][6] for ll in Gauss_params]})
+
 
 else:
     print 'CASA arguments config file not found!!'
+
+# CASA <43>: out.keys()
+#   Out[43]: ['timestamps', 'imagenames', 'succeeded', 'outputs']
+
+# CASA <44>: out['outputs'][0].keys()
+#   Out[44]: ['converged', 'deconvolved', 'results']
+
+# CASA <45>: out['outputs'][0]['deconvolved'].keys()
+#   Out[45]:
+# ['component66',
+#  'component39',
+#  'component38',
+#  'component37',
+#  'component36',
+#  .............]
+
+# CASA <42>: out['outputs'][0]['deconvolved']['component50']
+#   Out[42]:
+# {'beam': {'beamarcsec': {'major': {'unit': 'arcsec',
+#                                    'value': 80.77617645263672},
+#                          'minor': {'unit': 'arcsec',
+#                                    'value': 54.635799407958984},
+#                          'positionangle': {'unit': 'deg',
+#                                            'value': -20.626220703125}},
+#           'beampixels': 200.02533455860916,
+#           'beamster': 1.175370395548373e-07},
+#  'flux': {'error': array([ 3.29035864,  0.        ,  0.        ,  0.        ]),
+#           'polarisation': 'Stokes',
+#           'unit': 'Jy',
+#           'value': array([ 22.37381579,   0.        ,   0.        ,   0.        ])},
+#  'ispoint': False,
+#  'label': '',
+#  'peak': {'error': 2.2380235668176343,
+#           'unit': 'Jy/beam',
+#           'value': 20.558514700697245},
+#  'shape': {'direction': {'error': {'latitude': {'unit': 'arcsec',
+#                                                 'value': 4.268624648944596},
+#                                    'longitude': {'unit': 'arcsec',
+#                                                  'value': 5.503895283292848}},
+#                          'm0': {'unit': 'rad',
+#                                 'value': -0.003066262239961642}, * 180/pi*3600
+#                          'm1': {'unit': 'rad',
+#                                 'value': -0.0006359638389362692}, * 180/pi*3600
+#                          'refer': 'J2000',
+#                          'type': 'direction'},
+#            'majoraxis': {'unit': 'arcsec', 'value': 101.22624749838064},
+#            'majoraxiserror': {'unit': 'arcsec', 'value': 16.8409753696521},
+#            'minoraxis': {'unit': 'arcsec', 'value': 47.44776658169024},
+#            'minoraxiserror': {'unit': 'arcsec', 'value': 20.731554584768958},
+#            'positionangle': {'unit': 'deg', 'value': 70.38611353707329},
+#            'positionangleerror': {'unit': 'deg', 'value': 20.07539719062812},
+#            'type': 'Gaussian'},
+#  'spectrum': {'channel': 50,
+#               'frequency': {'m0': {'unit': 'GHz', 'value': 1.096},
+#                             'refer': 'TOPO',
+#                             'type': 'frequency'},
+#               'type': 'Constant'},
+#  'sum': {'unit': 'Jy/beam', 'value': 2194.9042081832886}}
--- DataBrowser/README.md
+++ DataBrowser/README.md
@@ -10,7 +10,7 @@ setenv SUNCASA "<your suncasa path>"
 setenv SUNCASADB "<your database path>"
 setenv SUNCASAPY46 "<your CASA 4.6 path>"
 setenv SUNCASAPY47 "<your CASA 4.7 path>"
-alias browser bokeh serve --show $SUNCASA/DataBrowser/EvtBrowser --port `netstat -atn | awk ' /tcp/ {printf("%s\\\\\\\\\\\\\\\\n",substr($4,index($4,":")+1,length($4) )) }' | sed -e "s/://g" | sort -rnu | awk '{array [$1] = $1} END {i=32768; again=1; while (again == 1) {if (array[i] == i) {i=i+1} else {print i; again=0}}}'`
+alias browser bokeh serve --show $SUNCASA/DataBrowser/EvtBrowser --port `(netstat  -atn | awk '{printf "%s\n%s\n", $4, $4}' | grep -oE '[0-9]*$'; seq 32768 61000) | sort -n | uniq -u | head -n 1`
 ```
 
 If in B-Shell, set your ~/.bashrc by adding:
@@ -19,7 +19,7 @@ export SUNCASA = "<your suncasa path>"
 export SUNCASADB = "<your database path>"
 export SUNCASAPY46 = "<your CASA 4.6 path>"
 export SUNCASAPY47 = "<your CASA 4.7 path>"
-alias browser='bokeh serve --show $SUNCASA/DataBrowser/EvtBrowser'
+alias browser='bokeh serve --show $SUNCASA/DataBrowser/EvtBrowser --port `port=32768; while netstat -atn | grep -q $port; do port=$(expr $port + 1); done; echo $port`'
 ```
 
 To launch the Databrowser, type in browser in your terminal.
