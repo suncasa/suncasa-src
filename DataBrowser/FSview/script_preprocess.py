07162bd542b66f1155d73ffa6f0e8952fc227af0
sjyu1988 <sjyu1988@gmail.com>
2016-12-04 14:11:18 -0500

    dspec_vector colormap changed

--- DataBrowser/FSview/script_preprocess.py
+++ DataBrowser/FSview/script_preprocess.py
@@ -8,7 +8,6 @@ msfile = 'SUN01_20141101.T163940-164700.50ms.cal.ms'
 ephemfile = 'horizons_sun_20141101.radecp'
 msinfofile = 'SUN01_20141101.T163940-164700.50ms.cal.msinfo.npz'
 
-
 ##################  ----------- U04-prep -----------------#########
 slfcalms_timeran = '16:46:12.375~16:46:32.475'
 slfcalms_s = ['0', '1', '2', '3']
@@ -16,42 +15,15 @@ slfcalms_chan = ['0~63', '0~63', '0~63', '0~63']
 structure_id = 'U04-prep'
 pol = 'LL'
 spwchan = ','.join('%s:%s' % t for t in zip(slfcalms_s, slfcalms_chan))
-phasecenter_local = 'slfj'
+phasecenter_local = 'J2000 14h26m58.891 -14d35m24.673'
 ##  ----------- spw 0 -----------------
-timerange = '2014/11/01/16:46:17.100~2014/11/01/16:46:17.600'
-freqrange = '1.086~1.106 GHz'
-CLNmask = 'slfcal/U04-prep/region_spw0.rgn'
-##  ----------- spw 1 -----------------
-# timerange = '2014/11/01/16:46:17.600~2014/11/01/16:46:18.400'
-# freqrange = '1.146~1.182 GHz'
-# CLNmask = 'slfcal/U04-prep/region_spw0.rgn'
-##  ----------- spw 2 -----------------
-# timerange = '2014/11/01/16:46:17.650~2014/11/01/16:46:18.250'
-# freqrange = '1.286~1.318 GHz'
-# CLNmask = 'slfcal/U04-prep/region_spw0.rgn'
-##  ----------- spw 4 -----------------
-# timerange = '2014/11/01/16:46:17.275~2014/11/01/16:46:19.450'
-# freqrange = '1.426~1.472 GHz'
-# CLNmask = 'slfcal/U04-prep/region_spw0.rgn'
-
-
-##################  ----------- typeIII01 -----------------#########
-slfcalms_timeran = '16:43:58.825~16:44:01.325'
-slfcalms_s = ['0', '1']
-slfcalms_chan = ['0~63', '0~63']
-structure_id = 'typeIII01-prep'
-pol = 'RR'
-spwchan = ','.join('%s:%s' % t for t in zip(slfcalms_s, slfcalms_chan))
-phasecenter_local = 'ljdfsldfj'
-# spwchan='0,1,2'
-##  ----------- spw 0 -----------------
-struct_timerange = '2014/11/01/16:44:00.975~2014/11/01/16:44:01.125'
-struct_freqrange = '1.040~1.072 GHz'
+struct_timerange = '2014/11/01/16:46:17.100~2014/11/01/16:46:17.600'
+struct_freqrange = '1.086~1.106 GHz'
 CLNmask = 'slfcal/{}/region_spw0.rgn'.format(structure_id)
 ##  ----------- spw 1 -----------------
-struct_timerange = '2014/11/01/16:44:00.475~2014/11/01/16:44:00.625'
-struct_freqrange = '1.160~1.208 GHz'
-CLNmask = 'slfcal/{}/region_spw0.rgn'.format(structure_id)
+# struct_timerange = '2014/11/01/16:46:17.600~2014/11/01/16:46:18.400'
+# struct_freqrange = '1.146~1.182 GHz'
+# CLNmask = 'slfcal/{}/region_spw0.rgn'.format(structure_id)
 ##  ----------- spw 2 -----------------
 # struct_timerange = '2014/11/01/16:46:17.650~2014/11/01/16:46:18.250'
 # struct_freqrange = '1.286~1.318 GHz'
@@ -61,24 +33,68 @@ CLNmask = 'slfcal/{}/region_spw0.rgn'.format(structure_id)
 # struct_freqrange = '1.426~1.472 GHz'
 # CLNmask = 'slfcal/{}/region_spw0.rgn'.format(structure_id)
 
-
-##################  ----------- ZP01 -----------------#########
-slfcalms_timeran = '16:41:02~16:41:26'
-slfcalms_s = ['0', '1']
-slfcalms_chan = ['0~63', '0~63']
-structure_id = 'ZP01-prep'
-pol = 'RRLL'
-spwchan = ','.join('%s:%s' % t for t in zip(slfcalms_s, slfcalms_chan))
-phasecenter_local = 'J2000 14h26m43.082 -14d37m43.817'
-##  ----------- spw 0 -----------------
-struct_timerange = '2014/11/01/16:41:11.125~2014/11/01/16:41:12.325'
-struct_freqrange =  '1.044~1.066 GHz'
-CLNmask = 'slfcal/{}/region_spw0.rgn'.format(structure_id)
-##  ----------- spw 1 -----------------
-# struct_timerange = '2014/11/01/16:41:12.575~2014/11/01/16:41:14.175'
-# struct_freqrange = '1.166~1.184 GHz'
+#
+# ##################  ----------- typeIII01 -----------------#########
+# slfcalms_timeran = '16:43:58.825~16:44:01.325'
+# slfcalms_s = ['0', '1']
+# slfcalms_chan = ['0~63', '0~63']
+# structure_id = 'typeIII01-prep'
+# pol = 'RR'
+# spwchan = ','.join('%s:%s' % t for t in zip(slfcalms_s, slfcalms_chan))
+# phasecenter_local = 'ljdfsldfj'
+# # spwchan='0,1,2'
+# ##  ----------- spw 0 -----------------
+# struct_timerange = '2014/11/01/16:44:00.975~2014/11/01/16:44:01.125'
+# struct_freqrange = '1.040~1.072 GHz'
 # CLNmask = 'slfcal/{}/region_spw0.rgn'.format(structure_id)
+# ##  ----------- spw 1 -----------------
+# struct_timerange = '2014/11/01/16:44:00.475~2014/11/01/16:44:00.625'
+# struct_freqrange = '1.160~1.208 GHz'
+# CLNmask = 'slfcal/{}/region_spw0.rgn'.format(structure_id)
+# ##  ----------- spw 2 -----------------
+# # struct_timerange = '2014/11/01/16:46:17.650~2014/11/01/16:46:18.250'
+# # struct_freqrange = '1.286~1.318 GHz'
+# # CLNmask = 'slfcal/{}/region_spw0.rgn'.format(structure_id)
+# ##  ----------- spw 4 -----------------
+# # struct_timerange = '2014/11/01/16:46:17.275~2014/11/01/16:46:19.450'
+# # struct_freqrange = '1.426~1.472 GHz'
+# # CLNmask = 'slfcal/{}/region_spw0.rgn'.format(structure_id)
+#
+#
+# ##################  ----------- ZP01 -----------------#########
+# slfcalms_timeran = '16:41:02~16:41:26'
+# slfcalms_s = ['0', '1']
+# slfcalms_chan = ['0~63', '0~63']
+# structure_id = 'ZP01-prep'
+# pol = 'RRLL'
+# spwchan = ','.join('%s:%s' % t for t in zip(slfcalms_s, slfcalms_chan))
+# phasecenter_local = 'J2000 14h26m43.082 -14d37m43.817'
+# ##  ----------- spw 0 -----------------
+# struct_timerange = '2014/11/01/16:41:11.125~2014/11/01/16:41:12.325'
+# struct_freqrange = '1.044~1.066 GHz'
+# CLNmask = 'slfcal/{}/region_spw0.rgn'.format(structure_id)
+# ##  ----------- spw 1 -----------------
+# # struct_timerange = '2014/11/01/16:41:12.575~2014/11/01/16:41:14.175'
+# # struct_freqrange = '1.166~1.184 GHz'
+# # CLNmask = 'slfcal/{}/region_spw0.rgn'.format(structure_id)
 
+
+# ##################  ----------- U02 -----------------#########
+# slfcalms_timeran = '16:45:39~16:45:48'
+# slfcalms_s = ['0', '1', '2']
+# slfcalms_chan = ['0~63', '0~63', '0~63']
+# structure_id = 'U02-prep'
+# pol = 'LL'
+# spwchan = ','.join('%s:%s' % t for t in zip(slfcalms_s, slfcalms_chan))
+# phasecenter_local = 'J2000 14h26m58.891 -14d35m24.673'
+# ##  ----------- spw 0 -----------------
+# struct_timerange = '2014/11/01/16:45:41.925~2014/11/01/16:45:42.225'
+# struct_freqrange = '1.042~1.060 GHz'
+# CLNmask = 'slfcal/{}/region_spw0.rgn'.format(structure_id)
+# ##  ----------- spw 1 -----------------
+# struct_timerange = '2014/11/01/16:45:40.875~2014/11/01/16:45:41.175'
+# struct_freqrange =  '1.168~1.196 GHz'
+# CLNmask = 'slfcal/{}/region_spw0.rgn'.format(structure_id)
 
 ''' ----- step 1 ----- '''
 
@@ -133,9 +149,6 @@ if prep:
         split()
         print 'split msfile to ' + slfcalms
 
-
-
-
 ms.open(slfcalms)
 
 axisInfo = ms.getdata(["axis_info"], ifraxis=True)
@@ -144,8 +157,6 @@ freqInfo = axisInfo["axis_info"]["freq_axis"]["chan_freq"].swapaxes(0, 1) / 1e9
 freqInfo_ravel = freqInfo.ravel()
 timeInfo = axisInfo["axis_info"]["time_axis"]['MJDseconds']
 timran = ms.range(["time"])
-
-
 
 refantenna = 'ea04'
 # antennas='0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26'
@@ -176,10 +187,11 @@ ms_spw = ['{}'.format(ll) for ll in xrange(freqIdx0[0], freqIdx1[0] + 1)]
 if len(ms_spw) == 1:
     ms_chan = ['{}~{}'.format(freqIdx0[1][0], freqIdx1[1][0])]
 else:
-    ms_chan = ['{}~{}'.format(freqIdx0[1][0], sz_freqInfo[1] - 1)] + ['0~{}'.format(sz_freqInfo[1] - 1) for ll in xrange(freqIdx0[0] + 1, freqIdx1[0])]
+    ms_chan = ['{}~{}'.format(freqIdx0[1][0], sz_freqInfo[1] - 1)] + ['0~{}'.format(sz_freqInfo[1] - 1) for ll in
+                                                                      xrange(freqIdx0[0] + 1, freqIdx1[0])]
     ms_chan.append('0~{}'.format(freqIdx1[1][0]))
 spw = ','.join('%s:%s' % t for t in zip(ms_spw, ms_chan))
-spwstr=spw
+spwstr = spw
 # spwstr = 'spw{}-{}'.format(slfcalms_s[0], slfcalms_s[-1])
 
 calprefix = 'caltables/'
@@ -197,9 +209,9 @@ imgprefix = imgprefix + structure_id + '_'
 
 # initial image and first slfcal table
 slfcal_img_global = imgprefix + timestr + 'T.' + spwstr + '.global.slfcal'
-slfcal_img_global=slfcal_img_global.replace(':', '-')
+slfcal_img_global = slfcal_img_global.replace(':', '-')
 slfcal_img_local = imgprefix + timestr + 'T.' + spwstr + '.local.slfcal'
-slfcal_img_local=slfcal_img_local.replace(':', '-')
+slfcal_img_local = slfcal_img_local.replace(':', '-')
 
 slfcal_img_list = [imgprefix + timestr + 'T.' + spwstr.replace(':', '-') + '.slfcal{:d}'.format(ll) for ll in range(4)]
 slfcal_table_list = [calprefix + '.slfcal.G{:d}'.format(ll) for ll in range(4)]
@@ -207,7 +219,7 @@ uvrange_list = ['<3klambda', '<3klambda', '<6klambda', '']
 # uvrange_list = ['<1klambda', '<3klambda', '<6klambda', '<9klambda', '','']
 
 slfcal_iter = 0
-clearcal(vis=slfcalms,spw=ms_spw[0])
+clearcal(vis=slfcalms, spw=ms_spw[0])
 tb.open(slfcalms, nomodify=False)
 colnames = tb.colnames()
 cols2rm = ["MODEL_DATA", 'CORRECTED_DATA']
@@ -226,6 +238,7 @@ timerange = timeran
 mode = 'mfs'
 imagermode = 'csclean'
 weighting = 'natural'
+antenna=antennas
 gain = 0.1
 niter = 500
 uvrange = uvrange_list[slfcal_iter]
@@ -246,22 +259,18 @@ interactive = True
 # interactive=False
 usescratch = True
 clean()
-os.system('rm -fr '+slfcal_img_list[slfcal_iter]+'.flux')
-# os.system('rm -fr '+slfcal_img_list[slfcal_iter]+'.mask')
-os.system('rm -fr '+slfcal_img_list[slfcal_iter]+'.model')
-os.system('rm -fr '+slfcal_img_list[slfcal_iter]+'.psf')
-os.system('rm -fr '+slfcal_img_list[slfcal_iter]+'.residual')
-
-
-
-
+os.system('rm -fr ' + slfcal_img_list[slfcal_iter] + '.flux')
+os.system('rm -fr '+slfcal_img_list[slfcal_iter]+'.mask')
+os.system('rm -fr ' + slfcal_img_list[slfcal_iter] + '.model')
+os.system('rm -fr ' + slfcal_img_list[slfcal_iter] + '.psf')
+os.system('rm -fr ' + slfcal_img_list[slfcal_iter] + '.residual')
 
 # gain solution, phase only
 gaincal(vis=slfcalms, refant=refantenna, caltable=slfcal_table_list[slfcal_iter], spw=spwstr, \
         selectdata=True, timerange=timeran, solint='inf', gaintype='G', calmode='p', minsnr=3)
 plotcal(caltable=slfcal_table_list[slfcal_iter], antenna=antennas, xaxis='antenna', yaxis='phase', \
         subplot=111, iteration='channel')
-clearcal(vis=slfcalms,spw = ms_spw[0])
+clearcal(vis=slfcalms, spw=ms_spw[0])
 applycal(vis=slfcalms, gaintable=slfcal_table_list[slfcal_iter], spw=ms_spw[0], selectdata=True, \
          antenna=antennas, interp='linear', flagbackup=False, applymode='calonly')
 
@@ -282,7 +291,6 @@ for slfcal_iter in range(1, 4):
     os.system('rm -fr ' + slfcal_img_list[slfcal_iter] + '.psf')
     os.system('rm -fr ' + slfcal_img_list[slfcal_iter] + '.residual')
 
-
     # gaincal(vis=slfcalms, refant=refantenna, caltable=slfcal_table_list[slfcal_iter], spw=spwstr, \
     #         selectdata=True, timerange=timeran, solint='inf', gaintype='G', calmode='p', \
     #         minsnr=3, gaintable=slfcal_table_list[0:slfcal_iter])
@@ -300,7 +308,7 @@ for slfcal_iter in range(1, 4):
     gaincal(vis=slfcalms, refant=refantenna, caltable=slfcal_table_list[slfcal_iter], spw=spwstr, \
             selectdata=True, timerange=timeran, solint='inf', gaintype='G', calmode='p', \
             minsnr=3)
-    clearcal(vis=slfcalms,spw=ms_spw[0])
+    clearcal(vis=slfcalms, spw=ms_spw[0])
     if os.path.exists(slfcal_table_list[slfcal_iter]):
         plotcal(caltable=slfcal_table_list[slfcal_iter], antenna=antennas, xaxis='antenna', yaxis='phase', \
                 subplot=111)
@@ -312,13 +320,12 @@ for slfcal_iter in range(1, 4):
                  antenna=antennas, interp='linear', flagbackup=False, applymode='calonly')
         break
 print 'self calibration finished within {:d} iterations'.format(slfcal_iter + 1)
-os.system('cp -r {} caltables/{}.spw{}.G'.format(slfcal_table_list[slfcal_iter],structure_id,ms_spw[0]))
+os.system('cp -r {} caltables/{}.spw{}.G'.format(slfcal_table_list[slfcal_iter], structure_id, ms_spw[0]))
 
 for ll in slfcal_table_list:
     os.system('rm -fr {}'.format(ll))
 for ll in slfcal_img_list:
     os.system('rm -fr {}.*'.format(ll))
-
 
 # final imaging
 tget('clean')
@@ -326,7 +333,7 @@ vis = slfcalms
 imagename = slfcal_img_global
 uvrange = ''
 # mask='rU01.mask2'
-# mask = 'slfcal/U04-prep/mask.spw0.rgn'
+mask = CLNmask
 niter = 200
 npercycle = 10
 imsize = [512, 512]
@@ -335,19 +342,19 @@ phasecenter = 'J2000 14h26m22.7351 -14d29m29.801'
 outertaper = ['50.0arcsec']
 interactive = False
 usescratch = False
-outertaper = ['50arcsec']
+# outertaper = ['50arcsec']
 clean()
-os.system('rm -fr ' + slfcal_img_global + '.flux')
-os.system('rm -fr ' + slfcal_img_global + '.mask')
-os.system('rm -fr ' + slfcal_img_global + '.model')
-os.system('rm -fr ' + slfcal_img_global + '.psf')
-os.system('rm -fr ' + slfcal_img_global + '.residual')
+os.system('rm -fr ' + slfcal_img_global + '*.flux')
+os.system('rm -fr ' + slfcal_img_global + '*.mask')
+os.system('rm -fr ' + slfcal_img_global + '*.model')
+os.system('rm -fr ' + slfcal_img_global + '*.psf')
+os.system('rm -fr ' + slfcal_img_global + '*.residual')
 
 tget('clean')
 vis = slfcalms
 imagename = slfcal_img_local
 uvrange = ''
-mask=CLNmask
+mask = CLNmask
 niter = 200
 npercycle = 10
 imsize = [128, 128]
@@ -368,6 +375,5 @@ caltabl = glob.glob('caltables/{}.spw?.G'.format(structure_id))
 for ll in caltabl:
     stridx1 = ll.index('spw')
     stridx2 = ll.index('.G')
-    applycal(vis=slfcalms, gaintable=ll, spw=ll[stridx1+3:stridx2], selectdata=True, \
+    applycal(vis=slfcalms, gaintable=ll, spw=ll[stridx1 + 3:stridx2], selectdata=True, \
              antenna=antennas, interp='linear', flagbackup=False, applymode='calonly')
-
