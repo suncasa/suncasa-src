535178cbc798f9200a70d793fc54fb7f9aa9a4ef
sjyu1988 <sjyu1988@gmail.com>
2017-02-16 09:42:38 -0500

    fix the log scaled display of Vector dynamic

--- DataBrowser/FSview/main.py
+++ DataBrowser/FSview/main.py
@@ -11,6 +11,7 @@ import matplotlib.cm as cm
 import matplotlib.colors as colors
 import numpy as np
 import pandas as pd
+from sys import platform
 import scipy.ndimage as sn
 import sunpy.map
 from math import radians, cos, sin
@@ -28,6 +29,29 @@ from puffin import PuffinMap
 
 __author__ = ["Sijie Yu"]
 __email__ = "sijie.yu@njit.edu"
+
+
+def getfreeport():
+    import socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('localhost', 0))
+    port = s.getsockname()[1]
+    s.close()
+    return port
+
+
+if platform == "linux" or platform == "linux2":
+    print 'Runing QLook in Linux platform'
+    for ll in xrange(5100, 5100 + 10):
+        os.system('fuser -n tcp -k {}'.format(ll))
+elif platform == "darwin":
+    print 'Runing QLook in OS X platform'
+    for ll in xrange(5100, 5100 + 10):
+        os.system(
+            'port=($(lsof -i tcp:{}|grep python2.7 |cut -f2 -d" ")); [[ -n "$port" ]] && kill -9 $port'.format(ll))
+        os.system('port=($(lsof -i tcp:{}|grep Google |cut -f2 -d" ")); [[ -n "$port" ]] && kill -9 $port'.format(ll))
+elif platform == "win32":
+    print 'Runing QLook in Windows platform'
 
 '''load config file'''
 suncasa_dir = os.path.expandvars("${SUNCASA}") + '/'
@@ -56,6 +80,12 @@ bokehpalette_viridis = [colors.rgb2hex(m) for m in colormap_viridis(np.arange(co
 '''
 -------------------------- panel 2,3   --------------------------
 '''
+
+
+def c_correlate(a, v):
+    a = (a - np.mean(a)) / (np.std(a) * len(a))
+    v = (v - np.mean(v)) / np.std(v)
+    return np.correlate(a, v, mode='same')
 
 
 def read_fits(fname):
@@ -215,6 +245,46 @@ def tab2_vdspec_update():
         tab2_bl_pol_cls_change(bl_index, select_pol)
         tab2_p_dspec.title.text = "Dynamic spectrum"
         tab2_Div_LinkImg_plot.text = ''
+
+
+ports = []
+
+
+def tab2_panel_XrsCorr_update():
+    global tab2_dspec_selected, dspecDF_select
+    if tab2_dspec_selected and len(tab2_dspec_selected) > 100:
+        time0, time1 = Time((dspecDF_select['time'].min() + timestart) / 3600. / 24., format='jd'), Time(
+            (dspecDF_select['time'].max() + timestart) / 3600. / 24., format='jd')
+        freq0, freq1 = dspecDF_select['freq'].min(), dspecDF_select['freq'].max()
+        timeidx0 = next(i for i in xrange(tab2_ntim) if tab2_tim[i] >= time0.mjd * 24. * 3600.)
+        timeidx1 = next(i for i in xrange(tab2_ntim - 1, -1, -1) if tab2_tim[i] <= time1.mjd * 24. * 3600.) + 1
+        freqidx0 = next(i for i in xrange(tab2_nfreq) if tab2_freq[i] >= freq0)
+        freqidx1 = next(i for i in xrange(tab2_nfreq - 1, -1, -1) if tab2_freq[i] <= freq1) + 1
+        dspecSel = tab2_r_dspec.data_source.data['image'][0][freqidx0:(freqidx1 + 1), timeidx0:(timeidx1 + 1)]
+        freqSel = tab2_freq[freqidx0:(freqidx1 + 1)]
+        timSel = tab2_tim[timeidx0:(timeidx1 + 1)]
+        nfreqSel, ntimSel = dspecSel.shape
+        ccpeak = np.empty((nfreqSel - 1, nfreqSel - 1))
+        ccpeak[:] = np.nan
+        ccmax = ccpeak.copy()
+        fidx1, fidx2 = [], []
+        for idx1 in xrange(1, nfreqSel):
+            for idx2 in xrange(0, idx1):
+                lightcurve1 = dspecSel[idx1, :]
+                lightcurve2 = dspecSel[idx2, :]
+                ccval = c_correlate(lightcurve1, lightcurve2)
+                ccmax[idx2, idx1 - 1] = np.amax(ccval)
+                ccpeak[idx2, idx1 - 1] = np.argmax(ccval)
+                fidx1.append(freqSel[idx1 - 1])
+                fidx2.append(freqSel[idx2])
+        CC_save = database_dir + event_id + struct_id + 'CC_save.npz'
+        np.savez(CC_save, spec=dspecSel, ccmax=ccmax, ccpeak=ccpeak, tim=timSel, freq=freqSel, nfreq=nfreqSel,
+                 ntim=ntimSel, fidx1=np.array(fidx1), fidx2=np.array(fidx2))
+        tab2_Div_LinkImg_plot.text = '<p><b>{}</b> saved.</p>'.format(CC_save)
+        port = getfreeport()
+        print 'bokeh serve {}DataBrowser/XrsCorr --show --port {} &'.format(suncasa_dir, port)
+        os.system('bokeh serve {}DataBrowser/XrsCorr --show --port {} &'.format(suncasa_dir, port))
+        ports.append(port)
 
 
 def tab2_update_dspec_image(attrname, old, new):
@@ -524,6 +594,14 @@ def tab2_dspec_selection_change(attrname, old, new):
             '{:.3f}'.format(dspecDF0.loc[DFidx_selected, :]['freq'])))
         tab2_Slider_time_LinkImg.value = tidx
         tab2_Slider_freq_LinkImg.value = fidx
+        if len(tab2_dspec_selected) > 100:
+            x0, x1 = dspecDF_select['time'].min(), dspecDF_select['time'].max()
+            y0, y1 = dspecDF_select['freq'].min(), dspecDF_select['freq'].max()
+            tab2_r_dspec_patch.data_source.data = ColumnDataSource(
+                pd.DataFrame({'xx': [x0, x1, x1, x0], 'yy': [y0, y0, y1, y1]})).data
+        else:
+            tab2_r_dspec_patch.data_source.data = ColumnDataSource(
+                pd.DataFrame({'xx': [], 'yy': []})).data
 
 
 def tab2_vla_square_selection_change(attrname, old, new):
@@ -861,6 +939,15 @@ def tab3_animate_onoff():
 
 def tab2_panel_exit():
     tab2_panel2_Div_exit.text = """<p><b>You may close the tab anytime you like.</b></p>"""
+    for ll in ports:
+        if platform == "linux" or platform == "linux2":
+            os.system('fuser -n tcp -k {}'.format(ll))
+        elif platform == "darwin":
+            os.system(
+                'port=($(lsof -i tcp:{}|grep python2.7 |cut -f2 -d" ")); [[ -n "$port" ]] && kill -9 $port'.format(ll))
+            os.system(
+                'port=($(lsof -i tcp:{}|grep Google |cut -f2 -d" ")); [[ -n "$port" ]] && kill -9 $port'.format(ll))
+        print 'port {} killed'.format(ll)
     raise SystemExit
 
 
@@ -1025,6 +1112,7 @@ tim_map = ((np.tile(tab2_tim, tab2_nfreq).reshape(tab2_nfreq, tab2_ntim) / 3600.
 freq_map = np.tile(tab2_freq, tab2_ntim).reshape(tab2_ntim, tab2_nfreq).swapaxes(0, 1)
 xx = tim_map.flatten()
 yy = freq_map.flatten()
+timestart = xx[0]
 fits_LOCL = config_EvtID['datadir']['fits_LOCL']
 fits_GLOB = config_EvtID['datadir']['fits_GLOB']
 fits_LOCL_dir = database_dir + event_id + struct_id + fits_LOCL
@@ -1125,6 +1213,7 @@ if os.path.exists(FS_dspecDF):
         tab2_p_dspec.axis.minor_tick_in = 3
         tab2_p_dspec.axis.major_tick_line_color = "white"
         tab2_p_dspec.axis.minor_tick_line_color = "white"
+        tab2_p_dspec.add_tools(BoxSelectTool(renderers=[tab2_r_square]))
 
         tab2_Select_pol = Select(title="Polarization:", value='I', options=['RR', 'LL', 'I', 'V'],
                                  width=config_plot['plot_config']['tab_FSview_base']['widgetbox_wdth'])
@@ -1501,6 +1590,11 @@ if os.path.exists(FS_dspecDF):
         tab2_LinkImg_HGHT = config_plot['plot_config']['tab_FSview_base']['vla_hght']
         tab2_LinkImg_WDTH = config_plot['plot_config']['tab_FSview_base']['vla_wdth']
 
+        tab2_panel2_BUT_XrsCorr = Button(label='Xcros Corr',
+                                         width=config_plot['plot_config']['tab_FSview_base']['widgetbox_wdth'],
+                                         button_type='warning')
+        tab2_panel2_BUT_XrsCorr.on_click(tab2_panel_XrsCorr_update)
+
         tab2_panel2_BUT_exit = Button(label='Exit FSview',
                                       width=config_plot['plot_config']['tab_FSview_base']['widgetbox_wdth'],
                                       button_type='danger')
@@ -1705,7 +1799,7 @@ if os.path.exists(FS_dspecDF):
                                   toolbar_location='right'), tab3_Div_Tb)
         lout3_3 = widgetbox(tab3_RBG_dspec_small, tab3_Slider_dspec_small_dmax, tab3_Slider_dspec_small_dmin,
                             tab3_BUT_dspec_small_reset, tab3_BUT_dspec_small_resetall, tab2_Select_aia_wave,
-                            tab2_panel3_BUT_exit,
+                            tab2_panel2_BUT_XrsCorr, tab2_panel3_BUT_exit,
                             # tab2_panel3_Div_exit,
                             width=200)
         panel3 = row(lout3_1, lout3_2, lout3_3)
@@ -1803,6 +1897,11 @@ if os.path.exists(FS_dspecDF):
                                                     config_plot['plot_config']['tab_FSview_base'][
                                                         'dspec_hght'] / float(tab2_nfreq) * spec_rs_step))
             tab2_p_dspec.add_tools(BoxSelectTool(renderers=[tab2_r_square]))
+
+            tab2_SRC_dspec_Patch = ColumnDataSource(pd.DataFrame({'xx': [], 'yy': []}))
+            tab2_r_dspec_patch = tab2_p_dspec.patch('xx', 'yy', source=tab2_SRC_dspec_Patch,
+                                                    fill_color=None, fill_alpha=0.5, line_color="Magenta",
+                                                    line_alpha=1.0, line_width=1)
 
             # tab2_p_dspec.border_fill_color = "silver"
             tab2_p_dspec.border_fill_alpha = 0.4
@@ -2112,6 +2211,11 @@ if os.path.exists(FS_dspecDF):
             for ctrl in tab2_CTRLs_LinkImg:
                 ctrl.on_change('value', tab3_slider_LinkImg_update)
 
+            tab2_panel2_BUT_XrsCorr = Button(label='Xcros Corr',
+                                             width=config_plot['plot_config']['tab_FSview_base']['widgetbox_wdth'],
+                                             button_type='warning')
+            tab2_panel2_BUT_XrsCorr.on_click(tab2_panel_XrsCorr_update)
+
             tab2_panel2_BUT_exit = Button(label='Exit FSview',
                                           width=config_plot['plot_config']['tab_FSview_base']['widgetbox_wdth'],
                                           button_type='danger')
@@ -2177,7 +2281,7 @@ if os.path.exists(FS_dspecDF):
             lout2_1_2 = row(column(row(tab2_p_dspec, tab2_p_dspec_yPro),
                                    tab2_p_dspec_xPro),
                             widgetbox(tab2_Select_pol, tab2_Select_bl,
-                                      tab2_Select_colorspace,
+                                      tab2_Select_colorspace, tab2_panel2_BUT_XrsCorr,
                                       tab2_panel2_BUT_exit, tab2_panel2_Div_exit,
                                       width=config_plot['plot_config']['tab_FSview_base']['widgetbox_wdth']))
             lout2_1 = column(lout2_1_1, lout2_1_2)
@@ -2251,7 +2355,7 @@ else:
                              'timestr': timestrs})
 
     rmax, rmin = tab2_spec_plt.max(), tab2_spec_plt.min()
-
+    # timestart = xx[0]
     TOOLS = "crosshair,pan,wheel_zoom,box_zoom,reset,save"
     downsample_dspecDF(spec_square_rs_tmax=spec_square_rs_tmax, spec_square_rs_fmax=spec_square_rs_fmax)
     tab2_SRC_dspec_square = ColumnDataSource(dspecDF0_rs)
@@ -2298,8 +2402,6 @@ else:
     tab2_p_dspec.axis.minor_tick_line_color = "white"
 
     tab2_dspec_selected = None
-
-    timestart = xx[0]
 
 
     def tab2_dspec_selection_change(attrname, old, new):
