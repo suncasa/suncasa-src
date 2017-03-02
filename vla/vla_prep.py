43172a46d0f3aab95dc4ca6c99ddbd7ebf80186c
Bin Chen <bin.chen@njit.edu>
2017-03-02 11:21:14 -0500

    fixed issue of vla_prep.py for backward compatibility with CASA 4.6.0 on header.update()

--- vla/vla_prep.py
+++ vla/vla_prep.py
@@ -454,7 +454,10 @@ def imreg(imagefile=None, fitsfile=None, beamfile=None, helio=None, \
         header['ctype1']='HPLN-TAN'
         header['ctype2']='HPLT-TAN'
         header['date-obs']=hel['date-obs']
-        header.update('exptime',hel['exptime'])
+        try:
+            header.update('exptime',hel['exptime'])
+        except:
+            header.append(('exptime',hel['exptime']))
         header.update('p_angle',hel['p0'])
         #header.update('comment', 'Fits header updated to heliocentric coordinates by Bin Chen')
         # update intensity units, i.e. to brightness temperature?
