295dafc4742533d8bf30febeb3b78c26df07dd62
sjyu1988 <sjyu1988@gmail.com>
2017-03-03 12:22:46 -0500

    aiaBrowser

---
+++ aiaBrowser/browser_bsh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -f
+#
+port=32768; while netstat -atn | grep -q $port; do port=$(expr $port + 1); done; echo $port
+echo Starting Bokeh server on port $port
+bokeh serve --show $SUNCASA/aiaBrowser --port $port
