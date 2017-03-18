600009ff2c58215bf923f713e09747fd02b5d443
sjyu1988 <sjyu1988@gmail.com>
2017-03-18 00:47:25 -0400

    add save and load button for cutslit add light-curve plot in stackplt

---
+++ aiaBrowser/MkPlot_csh.sh
@@ -0,0 +1,6 @@
+#!/bin/tcsh -f
+#
+set port = `(netstat  -atn | awk '{printf "%s\n%s\n", $4, $4}' | grep -oE '[0-9]*$'; seq 32768 61000) | sort -n | uniq -u | head -n 1`
+
+echo Starting Bokeh server on port $port
+bokeh serve --show $SUNCASA/aiaBrowser/MkPlot --port $port
