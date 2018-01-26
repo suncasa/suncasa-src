b8c180eafe2263b7987ecb10edaad370d1521b9e
sjyu1988 <sjyu1988@gmail.com>
2018-01-26 17:24:24 -0500

    add movie feathre to svplot

--- utils/svplot.py
+++ utils/svplot.py
@@ -8,7 +8,6 @@ import urllib2
 from split_cli import split_cli as split
 from ptclean_cli import ptclean_cli as ptclean
 from suncasa.utils import helioimage2fits as hf
-from suncasa.utils import dspec2 as ds
 import sunpy.map as smap
 from sunpy.net import vso
 from astropy import units as u
