2a27bcb497831c6a7c5650de9b8e6d0371ce2c51
sjyu1988 <sjyu1988@gmail.com>
2018-03-05 16:03:25 -0500

    add keyword timerange to movie feature in svplot

--- utils/svplot.py
+++ utils/svplot.py
@@ -173,7 +173,7 @@ def mk_qlook_image(vis, ncpu=10, timerange='', twidth=12, stokes='I,V', antenna=
     return imres
 
 
-def plt_qlook_image(imres, timerange='', figdir=None, specdata=None, verbose=True, stokes='I,V', fov=None, vmax=None, vmin=None):
+def plt_qlook_image(imres, timerange='', figdir=None, specdata=None, verbose=True, stokes='I,V', fov=None, imax=None, imin=None):
     from matplotlib import pyplot as plt
     from sunpy import map as smap
     from sunpy import sun
@@ -380,7 +380,7 @@ def plt_qlook_image(imres, timerange='', figdir=None, specdata=None, verbose=Tru
                     eomap.plot_settings['cmap'] = plt.get_cmap(cmaps[pol])
                     # import pdb
                     # pdb.set_trace()
-                    eomap.plot(axes=axs[n + nspw * pol], vmax=vmax, vmin=vmin)
+                    eomap.plot(axes=axs[n + nspw * pol], vmax=imax, vmin=imin)
                     eomap.draw_limb()
                     eomap.draw_grid()
                     ax = plt.gca()
@@ -461,7 +461,7 @@ def plt_qlook_image(imres, timerange='', figdir=None, specdata=None, verbose=Tru
         figname = observatory + '_qlimg_' + plttime.isot.replace(':', '').replace('-', '')[:19] + '.png'
         # fig_tdt = plttime.to_datetime())
         # fig_subdir = fig_tdt.strftime("%Y/%m/%d/")
-        figdir_ = figdir+'/'  # + fig_subdir
+        figdir_ = figdir + '/'  # + fig_subdir
         if not os.path.exists(figdir_):
             os.makedirs(figdir_)
         if verbose:
@@ -486,7 +486,7 @@ def dspec_external(vis, workdir='./', specfile=None):
 def svplot(vis, timerange=None, spw='', workdir='./', specfile=None, bl=None, uvrange=None, stokes='RR,LL', dmin=None, dmax=None, goestime=None,
            reftime=None, xycen=None, fov=[500., 500.], xyrange=None, restoringbeam=[''], robust=0.0, niter=500, imsize=[512], cell=['5.0arcsec'],
            interactive=False, usemsphacenter=True, imagefile=None, fitsfile=None, plotaia=True, aiawave=171, aiafits=None, savefig=False,
-           mkmovie=False, overwrite=True, ncpu=10, twidth=1, verbose=True, vmax=None, vmin=None):
+           mkmovie=False, overwrite=True, ncpu=10, twidth=1, verbose=True, imax=None, imin=None):
     '''
     Required inputs:
             vis: calibrated CASA measurement set
@@ -513,6 +513,7 @@ def svplot(vis, timerange=None, spw='', workdir='./', specfile=None, bl=None, uv
             imagefile: if imagefile provided, use it. Otherwise do clean and generate a new one.
             fitsfile: if fitsfile provided, use it. Otherwise generate a new one
             savefig: whether to save the figure
+            imax/imin: maximum/minimum value for radio image scaling
     Example:
 
     '''
@@ -677,8 +678,8 @@ def svplot(vis, timerange=None, spw='', workdir='./', specfile=None, bl=None, uv
                                            robust=robust, niter=niter, imsize=imsize, cell=cell, c_external=True)
             if not os.path.exists(qlookfigdir):
                 os.makedirs(qlookfigdir)
-            plt_qlook_image(imres, timerange=timerange, figdir=qlookfigdir, specdata=specdata, verbose=True, stokes=stokes, fov=xyrange, vmax=vmax,
-                            vmin=vmin)
+            plt_qlook_image(imres, timerange=timerange, figdir=qlookfigdir, specdata=specdata, verbose=True, stokes=stokes, fov=xyrange, imax=imax,
+                            imin=imin)
 
     else:
         spec = specdata['spec']
