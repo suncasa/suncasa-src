3f88ba22c879d8b8bfc2ab7f4ccf4677ce86b3f3
sjyu1988 <sjyu1988@gmail.com>
2020-01-04 22:27:40 -0500

    add EOVSA thumbnail image generating to pipeline

--- shellScript/pipeline_plt.bash
+++ shellScript/pipeline_plt.bash
@@ -5,7 +5,7 @@ export PYTHONPATH="/common/python/"
 export PATH="/common/anaconda2/bin:$PATH"
 source activate py3x
 
-if [$#argv >= 3]; then
+if ["$#" -ge 3]; then
     /common/anaconda2/envs/py3x/bin/python /common/python/eovsa/eovsa_pltQlookImage.py $argv
 else
     /common/anaconda2/envs/py3x/bin/python /common/python/eovsa/eovsa_pltQlookImage.py
