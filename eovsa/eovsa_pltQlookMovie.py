981fdf6a5c3bbea878ed9a7a8770cfb56473e3f7
sjyu1988 <sjyu1988@gmail.com>
2020-11-24 13:50:37 -0500

    update ant_trnage to check time range of visibility and that of when old 2-m antennas are supposed to be tracking. Choose no matter which is narrower

--- eovsa/eovsa_pltQlookMovie.py
+++ eovsa/eovsa_pltQlookMovie.py
@@ -89,7 +89,7 @@ def pltEovsaQlookImageSeries(timobjs, spw, vmax, vmin, aiawave, fig=None, axs=No
             spwstr = '-'.join(['{:02d}'.format(int(sp_)) for sp_ in sp.split('~')])
             t_hr = tmjd_hr[tidx]
             t_hr_st_blend = 0.0
-            t_hr_ed_blend = 6.0
+            t_hr_ed_blend = 7.0
             if t_hr < 8.0:
                 eofile = imgindir_prevday + 'eovsa_{}.spw{}.tb.disk.fits'.format(dateobj_prevday.strftime('%Y%m%d'),                                                                                 spwstr)
             else:
@@ -108,7 +108,7 @@ def pltEovsaQlookImageSeries(timobjs, spw, vmax, vmin, aiawave, fig=None, axs=No
 
             t_hr = tmjd_hr[tidx]
 
-            if t_hr_st_blend <= t_hr < t_hr_ed_blend:
+            if t_hr_st_blend <= t_hr <= t_hr_ed_blend:
                 eofile_nextday = imgindir + 'eovsa_{}.spw{}.tb.disk.fits'.format(
                     dateobj.strftime('%Y%m%d'), spwstr)
                 if not os.path.exists(eofile_nextday):
