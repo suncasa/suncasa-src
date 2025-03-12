8e5d1f9285d9098ed5c590f878b42fd2a257fc70
Sijie Yu <sjyu1988@gmail.com>
2025-03-12 10:02:45 -0400

    Refactor: Update main functions in eovsa_fitsutils.py and eovsa_pltQlookImage.py to use dateobj parameter and improve argument parsing

--- suncasa/eovsa/eovsa_pltQlookImage.py
+++ suncasa/eovsa/eovsa_pltQlookImage.py
@@ -609,7 +609,7 @@ if __name__ == '__main__':
     else:
         dateobj = Time(args.date).datetime
 
-    print(f"Running pipeline_plt for date {dateobj.strftime('%Y-%m-%dT%H:%M')}.")
+    print(f"Running pipeline_plt for date {dateobj.strftime('%Y-%m-%d')}.")
     print("Arguments:")
     print(f"  ndays: {args.ndays}")
     print(f"  clearcache: {args.clearcache}")
@@ -631,4 +631,4 @@ if __name__ == '__main__':
         debug=args.debug
     )
 
-    print(f"Running pipeline plot for date {t.strftime('%Y-%m-%dT%H:%M')}...")
+
